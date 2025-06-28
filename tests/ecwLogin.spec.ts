f2b74b92781f3edba9150ffbcd6744ccd8312212
mustafa-boorenie <75136994+mustafa-boorenie@users.noreply.github.com>
2025-06-27 19:54:33 -0500

    added providers checklist

--- tests/ecwLogin.spec.ts
+++ tests/ecwLogin.spec.ts
@@ -10,7 +10,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Web EMR Login Page/);
 });
 
-test('input username', async ({ page }) => {
+test('run script', async ({ page }) => {
 
 
   // LOG IN METHODS:
@@ -32,13 +32,52 @@ test('input username', async ({ page }) => {
   await page.keyboard.press('Enter');
 
   // ------------------------------------------------------------
-
+  
   // NAVIGATE TO RESOURCE SCHEDULER
+  await expect(page.locator('#jellybean-panelLink4')).toBeVisible();
+
+  await page.locator('#jellybean-panelLink4').click();
+
+  await page.locator('#Practice_menu').getByText('Practice').click();
+
+  await expect(page.locator('#RESOURCE_SCHEDULING_PRACTICE')).toBeVisible();
+
+  await page.locator('#RESOURCE_SCHEDULING_PRACTICE').click();
+
+  await expect(page.locator('#providerScroll')).toBeVisible();
+
+  // ------------------------------------------------------------
+
+    // If the alert is visible, click the close button
+    if (await page.locator('#alert').getByText('×').isVisible()) {
+      await page.locator('#alert').getByText('×').click();
+    } else {
+      console.log('No alert found');
+    }
+  
+
+  // ITERATE THROUGH PROVIDERS
+  const providers = await page.locator('#providerScroll .checkbox').count();
+
+  for (let i = 0; i < providers; i++) {
+
+    // Check on the checkbox
+    await page.locator('#providerScroll .checkbox').nth(i).click();
+
+    // Wait for the checkbox to be checked
+
+    console.log('Checked provider ' + i);
+
+    // // Uncheck the checkbox
+    // await page.locator('#providerScroll .checkbox').nth(i).click();
+  }
+
+
 
   // TODO: 
-  // 1) Get account not requiring 2FA
-  // 2) Navigate to resource scheduler
-  // 3) Find correct date and provider
-  // 4) Find correct visit
-  // 5) Turn into non-billable visit
+  // 1) Get correct date range
+  // 2) Get correct dates and providers
+  // 3) Go to the correct date and provider
+  // 4) Click on every visit within range
+  // 5) Turn each visit into non-billable visit
 });
